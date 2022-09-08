a0bb92f40189667931255974fb91294c2b3d8373
ShrimpandGits <44371907+ShrimpandGits@users.noreply.github.com>
2022-09-07 19:30:38 -0500

    Update nash_viz.py

--- nash_viz.py
+++ nash_viz.py
@@ -1,5 +1,10 @@
+#importing package - make sure auth token is correctly adjusted on github and in config file 
 from geojsonio import display
 
-with open('open_it.geojson') as f:
+
+#from the package, open Nashville District data 
+# found here: https://data.nashville.gov/General-Government/Council-Districts-Map-/mfy7-e5qx
+
+with open('nashville.geojson') as f:
     contents = f.read()
     display(contents)
